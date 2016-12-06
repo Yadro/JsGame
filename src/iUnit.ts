bb2be52719ad075a83136bdbcdeb7f3789ac5b30
yadro <yadrone@gmail.com>
2016-12-06 22:16:05 +0300

    refactoring canMove. Add RootUnit class

--- src/iUnit.ts
+++ src/iUnit.ts
@@ -1,10 +1,29 @@
 import {Characters} from "./characters";
 
+
 export abstract class IUnit {
   char: string;
-  pos: {x, y, dir};
+  pos: IPosition;
   field: any[][];
   units: Characters;
 
   abstract next();
+}
+
+export interface IPosition {
+  x;
+  y;
+  dir;
+  speed;
+}
+export class RootUnit {
+  char: string;
+  pos: IPosition;
+  field: any[][];
+  units: Characters;
+
+  constructor(char, pos) {
+    this.char = char;
+    this.pos = pos;
+  }
 }
--- src/unit.ts
+++ src/unit.ts
@@ -1,33 +1,37 @@
-import {IUnit} from "./iUnit";
+import {IUnit, IPosition, RootUnit} from "./iUnit";
 import {Bullet} from "./Bullet";
 import {check, clone} from "./tools";
 
-export class Unit extends IUnit {
-  char = 'A';
-  pos = {
-    x: 5,
-    y: 1,
-    dir: 1,
-    down: false
-  };
+export class Unit extends RootUnit {
   jump = 0;
   bullets = 5;
+  down = false;
+
+  constructor() {
+    super('A', {
+      x: 5,
+      y: 1,
+      dir: 1,
+      speed: 2,
+    });
+  }
 
   moveTo(e) {
     const keys = e.map(el => el.key);
     const codes = e.map(el => el.code);
-    const {x, y, down, dir} = this.pos;
+    let isMove = false;
+    const {down} = this;
+
     if (check(keys, 'a')) {
       this.pos.dir = -1;
-      if (this.field[y][x - 1] == 0) {
-        this.pos.x += dir;
-      }
+      isMove = true;
     }
     if (check(keys, 'd')) {
       this.pos.dir = 1;
-      if (this.field[y][x + 1] == 0) {
-        this.pos.x += dir;
-      }
+      isMove = true;
+    }
+    if (isMove) {
+      this.pos.x += this.canMove(this.pos);
     }
     if (check(keys, 'w')) {
       if (down) {
@@ -46,9 +50,9 @@ export class Unit extends IUnit {
     const {x, y} = this.pos;
     if (this.field[y + 1][x] == 0) {
       this.pos.y++;
-      this.pos.down = false;
+      this.down = false;
     } else {
-      this.pos.down = true;
+      this.down = true;
     }
     if (this.jump > 0) {
       if (this.field[y - 1][x] == 0) {
@@ -61,5 +65,27 @@ export class Unit extends IUnit {
     if (this.bullets < 5) {
       this.bullets++;
     }
+  }
+
+  private canMove(posObj : IPosition, isHorizont = true) {
+    const {dir, speed} = posObj;
+    const fixPos = isHorizont ? posObj.y : posObj.x;
+    const pos =    isHorizont ? posObj.x : posObj.y;
+
+    const isWall = (pos) => {
+      const query = isHorizont
+        ? this.field[fixPos][pos]
+        : this.field[pos][fixPos];
+      return query != 0;
+    };
+
+    let canPos = 0;
+    for (let i = pos + dir; Math.abs(pos - i) <= speed; i += dir) {
+      if (isWall(i)) {
+        return dir * canPos;
+      }
+      canPos++;
+    }
+    return dir * canPos;
   }
 }
