46a287784d8ef3f77f4a200ec71e77ab707ca680
yadro <yadrone@gmail.com>
2016-12-10 14:31:29 +0300

    fix bot

--- src/units/bot.ts
+++ src/units/bot.ts
@@ -3,19 +3,24 @@ import {RootUnit} from "./iUnit";
 export class Bot extends RootUnit {
   constructor() {
     super('x', {
-      x: 1,
-      y: 1,
-      dir: 1,
-      speed: 3
-    });
+      x: 100,
+      y: 150,
+      dirX: 1,
+      dirY: 0,
+      speedX: 3,
+      speedY: 3,
+      touch: false,
+    }, 32);
   }
 
 
   next() {
-    let delta = this.canMove(this.pos);
-    this.pos.x += delta;
-    if (!delta) {
-      this.pos.dir = -this.pos.dir;
+    const {x, y} = this.checkCollision(this.pos);
+    this.pos.x += x;
+    this.pos.y += y;
+
+    if (this.pos.touch) {
+      this.pos.dirX = -this.pos.dirX;
     }
   }
 }
