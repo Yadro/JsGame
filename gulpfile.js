b4b13c0112d6f35e5885b8d50bb661873057af51
yadro <yadrone@gmail.com>
2016-12-10 11:31:42 +0300

    browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,41 @@
 
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var sourcemaps = require('gulp-sourcemaps');
-var tsProject = ts.createProject('tsconfig.json');
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
+const tsProject = ts.createProject('tsconfig.json');
+const browserSync = require("browser-sync");
+const reload = browserSync.reload;
+const rimraf = require('rimraf');
+
+var config = {
+  server: {
+    baseDir: "./"
+  },
+  tunnel: false,
+  host: 'localhost',
+  port: 9000,
+  logPrefix: "Game"
+};
 
 gulp.task('ts', function () {
   return gulp.src('src/**/*.ts')
     .pipe(sourcemaps.init())
       .pipe(tsProject())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'))
+    .pipe(reload({stream: true}));
 });
 
 gulp.task('watch', function () {
   gulp.watch('src/**/*.ts', ['ts']);
 });
 
-gulp.task('default', ['ts', 'watch']);
+gulp.task('webserver', function () {
+  browserSync(config);
+});
+
+gulp.task('clean', function (cb) {
+  rimraf('build', cb);
+});
+
+gulp.task('default', ['webserver', 'ts', 'watch']);
