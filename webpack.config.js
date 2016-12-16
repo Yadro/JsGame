02f06451be191bc956c222a469e67afea9fd5ae1
yadro <yadrone@gmail.com>
2016-12-16 18:34:44 +0300

    json loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,12 @@ module.exports = {
     filename: 'build/bundle.js'
   },
   resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.json']
   },
   module: {
     loaders: [
-      {test: /\.tsx?$/, loader: 'ts-loader'}
+      {test: /\.tsx?$/, loader: 'ts-loader'},
+      {test: /\.json$/, loader: 'json'},
     ]
   },
   devtool: 'source-map',
